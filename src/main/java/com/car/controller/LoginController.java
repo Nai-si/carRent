18b966fac61fa6d48823b710c4cc2c7bc0131c51
Asika <862568059@qq.com>
2020-09-17 18:58:12 +0800

    获取登录用户信息并显示在前端页面

--- src/main/java/com/car/controller/LoginController.java
+++ src/main/java/com/car/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.car.controller;
 import com.car.common.JsonResult;
 import com.car.entity.User;
 import com.car.service.UserService;
+import com.car.utils.StrUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +22,31 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 用户登录
+     * @param tel
+     * @param password
+     * @param email
+     * @param session
+     * @return
+     */
     @RequestMapping("/login.do")
     @ResponseBody
     public JsonResult login(String tel, String password, String email, HttpSession session){
         User user = userService.login(tel, password);
-        session.setAttribute("loginUser",user);
+        session.setAttribute(StrUtils.LOGIN_USER,user);
         return new JsonResult(1,user);
+    }
+
+    @RequestMapping("/query.do")
+    @ResponseBody
+    public JsonResult query(HttpSession session){
+        User user = (User) session.getAttribute(StrUtils.LOGIN_USER);
+        if (user == null) {
+            return new JsonResult(0,"请先登录");
+        }else{
+            return new JsonResult(1,user);
+        }
     }
 
 
