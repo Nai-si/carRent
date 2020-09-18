adf4cf0a905f0efeddac32786f2dc3b2704b9942
Asika <862568059@qq.com>
2020-09-18 13:26:37 +0800

    完成了城市信息的获取

---
+++ src/main/java/com/car/utils/DistrictUtils.java
@@ -0,0 +1,46 @@
+package com.car.utils;
+
+/**
+ * @ProjectName: carRent
+ * @Author: Jsx
+ * @Time: 2020/9/18 11:06
+ * @Description:
+ */
+public class DistrictUtils {
+    public static Integer quiz1;
+    public static Integer quiz2;
+    public static Integer quiz3;
+    public static Integer quiz4;
+
+    public static Integer getQuiz1() {
+        return quiz1;
+    }
+
+    public static void setQuiz1(Integer quiz1) {
+        DistrictUtils.quiz1 = quiz1;
+    }
+
+    public static Integer getQuiz2() {
+        return quiz2;
+    }
+
+    public static void setQuiz2(Integer quiz2) {
+        DistrictUtils.quiz2 = quiz2;
+    }
+
+    public static Integer getQuiz3() {
+        return quiz3;
+    }
+
+    public static void setQuiz3(Integer quiz3) {
+        DistrictUtils.quiz3 = quiz3;
+    }
+
+    public static Integer getQuiz4() {
+        return quiz4;
+    }
+
+    public static void setQuiz4(Integer quiz4) {
+        DistrictUtils.quiz4 = quiz4;
+    }
+}
